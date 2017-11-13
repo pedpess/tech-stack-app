5ca7bdea4dd91e4d13a2275368105b81d06c94b4
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-11-13 23:47:31 +0200

    Add common components

---
+++ src/components/common/Button.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ onPress, children }) => {
+  const { buttonStyle, textStyle } = styles;
+  return (
+    <TouchableOpacity style={buttonStyle} onPress={onPress}>
+      <Text style={textStyle}>{children}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: '#007aff',
+    borderRadius: 5,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+  }
+};
+
+export { Button };
