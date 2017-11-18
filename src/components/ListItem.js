1a6a53fa89ec384e048ef07c28d36101cf60115d
Pedro Pessoa <pessoa.pedro91@gmail.com>
2017-11-19 01:35:46 +0200

    Create select items action

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
+import { connect } from 'react-redux';
 import { CardItem } from './common';
+import * as actions from '../actions/index';
 
 class ListItem extends Component {
 
   render() {
+    console.log(this.props);
     return (
-      <CardItem>
-        <Text>{this.props.library.title}</Text>
-      </CardItem>
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(this.props.library.id)}
+      >
+        <View>
+          <CardItem>
+            <Text style={styles.titleStyle}>{this.props.library.title}</Text>
+          </CardItem>
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
 
-export default ListItem;
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 18,
+  }
+};
+
+export default connect(null, actions)(ListItem);
